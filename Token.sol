fee49bebc3f783bf1e99af4f87feb4ae322af3cc
AHAMADA <faouzi.ahamada@gmail.com>
2025-11-08 20:23:34 +0100

    Add files via upload

---
+++ Token.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Token {
+    string public name = 'MoonToken';
+    string public symbol = 'MNT';
+    uint256 public totalSupply = 1000000;
+}
